66e741baf4bfbbb100db392a4e6bf7a3d750aab7
wentingrohwer <wenting.rohwer@gmail.com>
2015-03-23 22:44:50 -0700

    Majority Element

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,13 +1,11 @@
-// Majority Element
-// Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+/*Majority Element
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
-// You may assume that the array is non-empty and the majority element always exist in the array.
+You may assume that the array is non-empty and the majority element always exist in the array.
 
-
-
-//Code 2 is works for a more general case : find the element that has the most occurance.
-//since we could make use of the key fact that this element occures more than 2/n, thus there is only one.
-
+Code 2 is works for a more general case : find the element that has the most occurance.
+since we could make use of the key fact that this element occures more than 2/n, thus there is only one.
+*/
 public class Solution {
     public int majorityElement(int[] num) {
         HashMap<Integer, Integer> result = new HashMap<Integer, Integer>();
@@ -25,22 +23,24 @@ public class Solution {
         return -1;
     }
 }
-// code 2
-// public int majorityElement(int[] num) {
-//         HashMap<Integer, Integer> result = new HashMap<Integer, Integer>();
-//         int count = 1;
-//         int max =0;
-//         int index = num[0];
-//         for ( int i = 0 ; i < num.length; i ++) {
-//             if (result.containsKey(num[i]) == false) {
-//                 result.put(num[i], count);
-//             } else {
-//                 result.put(num[i], result.get(num[i]) + 1);
-//             }
-//             if (result.get(num[i]) > max) {
-//                 max = result.get(num[i]);
-//                 index = i;
-//             }
-//         }
-//         return num[index];
-//     }
+/*
+code 2
+public int majorityElement(int[] num) {
+        HashMap<Integer, Integer> result = new HashMap<Integer, Integer>();
+        int count = 1;
+        int max =0;
+        int index = num[0];
+        for ( int i = 0 ; i < num.length; i ++) {
+            if (result.containsKey(num[i]) == false) {
+                result.put(num[i], count);
+            } else {
+                result.put(num[i], result.get(num[i]) + 1);
+            }
+            if (result.get(num[i]) > max) {
+                max = result.get(num[i]);
+                index = i;
+            }
+        }
+        return num[index];
+    }
+*/
